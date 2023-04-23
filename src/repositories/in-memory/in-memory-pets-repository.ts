3bd6d10dd5d62c93ebe3a5b22aa7ef9b2935d9da
Pedro Bovolin D' Avila <pedrodavila.bovolin@gmail.com>
2023-04-23 01:13:36 -0300

    update fetch many by city method

--- src/repositories/in-memory/in-memory-pets-repository.ts
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -1,14 +1,48 @@
 import { randomUUID } from 'node:crypto'
-import { PetsRepository } from '../pets-repository'
+import { PetsRepository, SearchParams } from '../pets-repository'
 import { Prisma, Pet } from '@prisma/client'
 
 export class InMemoryPetsRepository implements PetsRepository {
   public pets: Pet[] = []
 
-  async fetchManyByCity(city: string, page: number) {
-    const pets = this.pets
-      .filter((pet) => pet.city === city)
-      .slice((page - 1) * 20, page * 20)
+  async fetchManyByCity(
+    {
+      city,
+      age,
+      behavior,
+      energyLevel,
+      requirements,
+      sex,
+      size,
+      species,
+    }: SearchParams,
+    page: number,
+  ) {
+    let pets = this.pets.filter((pet) => pet.city === city)
+
+    if (age) {
+      pets = pets.filter((pet) => pet.age === age)
+    }
+    if (sex) {
+      pets = pets.filter((pet) => pet.sex === sex)
+    }
+    if (energyLevel) {
+      pets = pets.filter((pet) => pet.energy_level === energyLevel)
+    }
+    if (species) {
+      pets = pets.filter((pet) => pet.species === species)
+    }
+    if (size) {
+      pets = pets.filter((pet) => pet.size === size)
+    }
+    if (behavior) {
+      pets = pets.filter((pet) => pet.behavior === behavior)
+    }
+    if (requirements) {
+      pets = pets.filter((pet) => pet.requirements === requirements)
+    }
+
+    pets = pets.slice((page - 1) * 20, page * 20)
 
     return pets
   }
--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -1,6 +1,25 @@
-import { Prisma, Pet } from '@prisma/client'
+import {
+  Prisma,
+  Pet,
+  EnergyLevel,
+  Age,
+  Sex,
+  Size,
+  Behavior,
+} from '@prisma/client'
+
+export interface SearchParams {
+  city: string
+  species?: string
+  requirements?: string[]
+  energyLevel?: EnergyLevel
+  age?: Age
+  sex?: Sex
+  size?: Size
+  behavior?: Behavior[]
+}
 
 export interface PetsRepository {
-  fetchManyByCity(city: string, page: number): Promise<Pet[]>
+  fetchManyByCity(searchParams: SearchParams, page: number): Promise<Pet[]>
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
 }
